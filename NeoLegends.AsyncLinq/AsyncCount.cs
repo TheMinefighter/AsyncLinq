ce80269e92eaf8eec1a536794e2ad46f4f2f114e
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-09-21 17:44:35 +0200

    Implementing Count with async predicate

--- NeoLegends.AsyncLinq/AsyncCount.cs
+++ NeoLegends.AsyncLinq/AsyncCount.cs
@@ -31,5 +31,34 @@ namespace System.Linq
 
             return (await Task.WhenAll(collection).ConfigureAwait(false)).Count(predicate);
         }
+
+        public static async Task<int> CountAsync<T>(this Task<IEnumerable<T>> collection, Func<T, Task<bool>> predicate)
+        {
+            Contract.Requires<ArgumentNullException>(collection != null);
+            Contract.Requires<ArgumentNullException>(predicate != null);
+
+            IEnumerable<T> evaluatedCollection = await collection.ConfigureAwait(false);
+            int count = 0;
+            foreach (T item in evaluatedCollection) {
+                if (await predicate(item).ConfigureAwait(false)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static async Task<int> CountAsync<T>(this IEnumerable<Task<T>> collection, Func<T, Task<bool>> predicate)
+        {
+            Contract.Requires<ArgumentNullException>(collection != null);
+            Contract.Requires<ArgumentNullException>(predicate != null);
+
+            int count = 0;
+            foreach (Task<T> item in collection) {
+                if (await predicate(await item.ConfigureAwait(false)).ConfigureAwait(false)) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
