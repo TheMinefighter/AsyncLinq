4d0767906fe1ddfa5f844cf989b6c4cc5c5559b5
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-09-21 19:11:37 +0200

    Changed message of exception to the thrown by the original LINQ method

--- NeoLegends.AsyncLinq/AsyncLast.cs
+++ NeoLegends.AsyncLinq/AsyncLast.cs
@@ -38,7 +38,7 @@ namespace System.Linq
                 }
             }
 
-            throw new InvalidOperationException("No element matched the predicate or the collection was empty.");
+            throw new InvalidOperationException("Sequence contains no matching element");
         }
 
         public static async Task<T> LastFinishedAsync<T>(this IEnumerable<Task<T>> collection)
