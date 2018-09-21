4b8ebe8a3666b70171f1f6739d90f5d3f8ac9299
Tobias Brohl aka The_Minefighter <tobrohl@gmail.com>
2018-09-21 21:04:52 +0200

    Reverting useless change

--- NeoLegends.AsyncLinq/AsyncAll.cs
+++ NeoLegends.AsyncLinq/AsyncAll.cs
@@ -17,14 +17,6 @@ namespace System.Linq
             return (await collection.ConfigureAwait(false)).All(predicate);
         }
 
-        public static async Task<bool> AllAsync<T>(this IEnumerable<Task<T>> collection, Func<T, bool> predicate)
-        {
-            Contract.Requires<ArgumentNullException>(collection != null);
-            Contract.Requires<ArgumentNullException>(predicate != null);
-
-            return (await Task.WhenAll(collection).ConfigureAwait(false)).All(predicate);
-        }
-
         public static async Task<bool> AllAsync<T>(this Task<IEnumerable<T>> collection, Func<T, Task<bool>> predicate)
         {
             Contract.Requires<ArgumentNullException>(collection != null);
@@ -38,6 +30,14 @@ namespace System.Linq
                 }
             }
             return true;
+        }
+
+        public static async Task<bool> AllAsync<T>(this IEnumerable<Task<T>> collection, Func<T, bool> predicate)
+        {
+            Contract.Requires<ArgumentNullException>(collection != null);
+            Contract.Requires<ArgumentNullException>(predicate != null);
+
+            return (await Task.WhenAll(collection).ConfigureAwait(false)).All(predicate);
         }
 
         public static async Task<bool> AllAsync<T>(this IEnumerable<Task<T>> collection, Func<T, Task<bool>> predicate)
